93fca29bbef5d2ef14b96edcd6f5b6422a01a346
stacyburris <stacy1burris@gmail.com>
2021-04-29 16:22:24 -0700

    updated readme

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
-
+import omdbApi from "./db.js";
+//import { useEffect, useState, useCallback } from "react";
+//import query from "./Query";
 
 function App() {
+  
   return (
     <div className="App">
 
